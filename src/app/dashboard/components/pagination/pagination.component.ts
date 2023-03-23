755833de2f8d56f8a7ad738ae6ca19dd11e4f617
DmitriiBiblev <biblev1608@gmail.com>
2023-03-23 13:42:39 +0300

    enhancement pagination

--- src/app/dashboard/components/pagination/pagination.component.ts
+++ src/app/dashboard/components/pagination/pagination.component.ts
@@ -1,11 +1,24 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PaginationComponent {
+  page: number = 1;
 
+  @Input() maxPages!: number;
+  @Output() onChangePage: EventEmitter<number> = new EventEmitter<number>();
+
+  onPrevPage() {
+    this.page -= 1;
+    this.onChangePage.emit(this.page);
+  }
+
+  onNextPage() {
+    this.page += 1;
+    this.onChangePage.emit(this.page);
+  }
 }
